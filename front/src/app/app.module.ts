5570775cf654c68ea8151ae61b516e272f37bb48
mbeaatriz <mbeaatriz@gmail.com>
2023-11-04 23:00:47 -0300

    Removendo nav

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -9,8 +9,6 @@ import { AutowppLabelComponent } from './atoms/autowpp-label/autowpp-label.compo
 import { AutowppInputComponent } from './atoms/autowpp-input/autowpp-input.component';
 import { AutowppHeaderComponent } from './organisms/autowpp-header/autowpp-header.component';
 import { AutowppLoginComponent } from './pages/autowpp-login/autowpp-login.component';
-import { AutowppNavComponent } from './organisms/autowpp-nav/autowpp-nav.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -25,8 +23,6 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     AppRoutingModule,
     AutowppButtonComponent,
     BrowserAnimationsModule,
-    AutowppNavComponent,
-    MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
